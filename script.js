6984540eac94b2b9a2c394afdd22706774577566
tasnimh22 <tasnimh4@gmail.com>
2016-12-14 22:56:33 +0000

    i changed my background and profile and i added a pun.

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ $(document).ready(function(){
         $("button").click(function() {
       $("#like").html("You like this project!");
     });
-    
+    $("#joke-1").click( function(){
+   // Task 1: use .html to change the text in joke-1 to "Nacho Cheese"
+   $("#joke-1").html("nacho cheese");
+});
+
+$("#joke-2").click( function(){
+   // Task 2: use .append to add a paragraph with "It gets jalapeño face!" in it.
+$("#joke-2").append("It gets jalapeño face!");
+});
+
+// Task 3: Create a click handler that appends a paragraph with "It was two-tired" in it.
+$("#joke-3").click( function(){
+   
+$("#joke-3").append("It was two-tired");
+});
+
+
+
+// Task 4: Create a click handler for joke-4 that changes the attribute src from the current image to this image --> "http://i.imgur.com/ZCIkkze.jpg"
+$("#joke-4").attr("src","https://i.imgur.com/ZCIkkze.jpg");
     
     
     
